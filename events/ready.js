57edc455ba296c5796c9ecaa19d92759faec98cc
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-12 18:10:32 +0500

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,24 @@ const { ActivityType } = require("discord.js");
 module.exports = (client) => {
   console.log(`🤖 Logged in as ${client.user.tag}`);
 
-  // STATUS SYSTEM
-  client.user.setPresence({
-    activities: [
-      {
-        name: "Made By Huztro",
-        type: ActivityType.Watching
-      }
-    ],
-    status: "online"
-  });
+  const statuses = [
+    { name: "Made By Huztro", type: ActivityType.Watching },
+    { name: "Moderating Premium Servers", type: ActivityType.Playing },
+    { name: "Ensuring Uptime Stability", type: ActivityType.Watching },
+    { name: "Exculting System Diagnostics", type: ActivityType.Listening },
+    { name: "Optimizing Perfomance Modules", type: ActivityType.Competing }
+  ];
+
+  let i = 0;
+
+  setInterval(() => {
+    const status = statuses[i];
+
+    client.user.setPresence({
+      activities: [status],
+      status: "online"
+    });
+
+    i = (i + 1) % statuses.length;
+  }, 5000);
 };
