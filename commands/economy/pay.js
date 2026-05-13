a6156f187683def6d7ef0167795f712f0b84637d
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 00:45:15 +0500

    pay.js

---
+++ commands/economy/pay.js
@@ -0,0 +1,42 @@
+const {
+  loadData,
+  saveData,
+  getUser
+} = require("../../utils/economy");
+
+module.exports = {
+  name: "pay",
+
+  async execute(message, args) {
+
+    const member =
+      message.mentions.users.first();
+
+    const amount =
+      parseInt(args[1]);
+
+    if (!member || isNaN(amount))
+      return;
+
+    const data = loadData();
+
+    const user =
+      getUser(data, message.author.id);
+
+    const target =
+      getUser(data, member.id);
+
+    if (amount > user.cash)
+      return message.reply("Not enough cash.");
+
+    user.cash -= amount;
+    target.cash += amount;
+
+    saveData(data);
+
+    message.channel.send(
+      `💸 Paid ${amount} coins`
+    );
+
+  }
+};
