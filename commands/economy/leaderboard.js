12e9479a58d2d7e04fbc2bce077711059f190317
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 00:46:44 +0500

    leaderboard.js

---
+++ commands/economy/leaderboard.js
@@ -0,0 +1,39 @@
+const {
+  loadData
+} = require("../../utils/economy");
+
+module.exports = {
+  name: "leaderboard",
+  aliases: ["lb"],
+
+  async execute(message) {
+
+    const data = loadData();
+
+    const top =
+      Object.entries(data)
+      .sort(
+        (a, b) =>
+        (b[1].cash + b[1].bank)
+        -
+        (a[1].cash + a[1].bank)
+      )
+      .slice(0, 10);
+
+    let text = "🏆 Leaderboard\n\n";
+
+    for (let i = 0; i < top.length; i++) {
+
+      const user =
+        await message.client.users.fetch(
+          top[i][0]
+        );
+
+      text += `${i + 1}. ${user.username}\n`;
+
+    }
+
+    message.channel.send(text);
+
+  }
+};
