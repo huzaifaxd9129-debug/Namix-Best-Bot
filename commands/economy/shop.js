d13202329863f6123ccbc37723979acfccc5e8da
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 00:48:45 +0500

    shop.js

---
+++ commands/economy/shop.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "shop",
+
+  async execute(message) {
+
+    message.channel.send(`
+🛒 SHOP
+
+Vip Rank - 10000
+Master Rank - 50000
+Eco Mastery Rank — 100000
+    `);
+
+  }
+};
