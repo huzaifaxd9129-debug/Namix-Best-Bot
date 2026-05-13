ac0d401773bbc5d39c9e60c449b163aa7abaaab7
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 02:25:09 +0500

    chatbot-disable.js

---
+++ commands/chatbot-disable.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+module.exports = {
+  name: "chatbot-disable",
+
+  execute(message) {
+
+    const file = "./chatbot.json";
+    let data = fs.existsSync(file)
+      ? JSON.parse(fs.readFileSync(file))
+      : {};
+
+    if (!data[message.guild.id]) {
+      return message.reply("❌ Setup chatbot first");
+    }
+
+    data[message.guild.id].enabled = false;
+
+    fs.writeFileSync(file, JSON.stringify(data, null, 2));
+
+    message.reply("❌ Chatbot disabled");
+  }
+};
