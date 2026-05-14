446d858afcb1fe1e4870fb1cb39a55d53d367162
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:06:03 +0500

    afk.js

--- commands/utility/afk.js
+++ commands/utility/afk.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: "afk",
 
+  async execute(message, args, client) {
+    client.afk = client.afk || new Map();
+    client.afk.set(message.author.id, args.join(" ") || "AFK");
+
+    message.reply("You are now AFK.");
+  }
+};
