13bd8b36b41f4d6a37779aa432be860342c6f017
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:01:07 +0500

    editsnipe.js

--- commands/utility/editsnipe.js
+++ commands/utility/editsnipe.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: "editsnipe",
 
+  async execute(message, args, client) {
+    const data = client.editsnipes?.get(message.channel.id);
+    if (!data) return message.reply("Nothing edited.");
+
+    message.reply(`Before: ${data.before}\nAfter: ${data.after}`);
+  }
+};
