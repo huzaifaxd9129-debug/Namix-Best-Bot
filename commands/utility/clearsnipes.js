0f3ff29fed4b332c5f15f6ede664ff966458ce5e
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:01:36 +0500

    clearsnipes.js

--- commands/utility/clearsnipes.js
+++ commands/utility/clearsnipes.js
@@ -1,1 +1,9 @@
+module.exports = {
+  name: "clearsnipes",
 
+  async execute(message, args, client) {
+    client.snipes?.delete(message.channel.id);
+    client.editsnipes?.delete(message.channel.id);
+    message.reply("Snipes cleared.");
+  }
+};
