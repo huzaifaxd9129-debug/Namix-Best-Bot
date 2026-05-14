5ace990bf37b4e30afd1be802b38e4f43c9a677b
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:02:34 +0500

    bots.js

--- commands/utility/bots.js
+++ commands/utility/bots.js
@@ -1,1 +1,8 @@
+module.exports = {
+  name: "bots",
 
+  async execute(message) {
+    const bots = message.guild.members.cache.filter(m => m.user.bot);
+    message.reply(`Bots: ${bots.size}`);
+  }
+};
