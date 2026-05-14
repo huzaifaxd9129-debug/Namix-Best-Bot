c4c586a0a1f5e08dd9d9039298d9a8bc92704999
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:05:24 +0500

    fristmessage.js

--- commands/utility/firstmessage.js
+++ commands/utility/firstmessage.js
@@ -1,1 +1,9 @@
+module.exports = {
+  name: "firstmessage",
 
+  async execute(message) {
+    const msgs = await message.channel.messages.fetch({ limit: 1, after: 0 });
+    const first = msgs.first();
+    message.reply(`First message: ${first.content}`);
+  }
+};
