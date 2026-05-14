4e268c8ee341dafe556363089568c46d82ab3de0
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 20:59:01 +0500

    serverbanner.js

--- commands/utility/serverbanner.js
+++ commands/utility/serverbanner.js
@@ -1,1 +1,9 @@
+module.exports = {
+  name: "serverbanner",
 
+  async execute(message) {
+    const banner = message.guild.bannerURL({ size: 4096 });
+    if (!banner) return message.reply("No banner set.");
+    message.reply(banner);
+  }
+};
