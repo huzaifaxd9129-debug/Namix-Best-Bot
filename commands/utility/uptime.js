2c23ff9e7dd931ec96efe3ce44e29604e905624e
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 20:55:29 +0500

    uptime.js

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -1,1 +1,9 @@
+module.exports = {
+  name: "uptime",
+  description: "Bot uptime",
 
+  async execute(message, args, client) {
+    const uptime = process.uptime();
+    message.reply(`Uptime: ${Math.floor(uptime)} seconds`);
+  }
+};
