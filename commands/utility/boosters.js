dcb5e6679257511214af9fe47a57b763b17a5177
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:02:08 +0500

    boosters.js

--- commands/utility/boosters.js
+++ commands/utility/boosters.js
@@ -1,1 +1,8 @@
+module.exports = {
+  name: "boosters",
 
+  async execute(message) {
+    const boosters = message.guild.members.cache.filter(m => m.premiumSince);
+    message.reply(`Boosters: ${boosters.size}`);
+  }
+};
