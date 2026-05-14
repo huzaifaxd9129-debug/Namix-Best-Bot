d9fbd1ee6744a6b7c733e9d92f002793233f4507
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:02:58 +0500

    roles.js

--- commands/utility/roles.js
+++ commands/utility/roles.js
@@ -1,1 +1,8 @@
+module.exports = {
+  name: "roles",
 
+  async execute(message) {
+    const roles = message.guild.roles.cache.map(r => r.name).slice(0, 20).join(", ");
+    message.reply(`Roles: ${roles}`);
+  }
+};
