1f8272a48144f432f750ac8bc51aaa4f1296adb1
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 22:06:25 +0500

    variables.js

--- commands/welcome/variables.js
+++ commands/welcome/variables.js
@@ -1,1 +1,19 @@
+module.exports = {
+  name: "welcome",
 
+  async execute(message, args) {
+    if (args[0] !== "variables") return;
+
+    message.reply(
+`📘 **Welcome Variables**
+
+{user} → Mention user
+{username} → Username
+{server} → Server name
+{members} → Member count
+
+Example:
+Welcome {user} to {server}! 🎉`
+    );
+  }
+};
