e4a085f96642eba5e3536ac60ad02448f4af5a67
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:03:26 +0500

    inrole.js

--- commands/utility/inrole.js
+++ commands/utility/inrole.js
@@ -1,1 +1,11 @@
+module.exports = {
+  name: "inrole",
 
+  async execute(message, args) {
+    const role = message.mentions.roles.first();
+    if (!role) return message.reply("Mention a role!");
+
+    const members = role.members.map(m => m.user.tag).slice(0, 10).join("\n");
+    message.reply(`Users in role:\n${members}`);
+  }
+};
