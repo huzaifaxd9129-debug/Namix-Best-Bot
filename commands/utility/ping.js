ae74c367e3427bc528a424715edca2c132947b87
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 20:54:45 +0500

    ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,1 +1,9 @@
+module.exports = {
+  name: "ping",
+  description: "Bot latency",
 
+  async execute(message, args, client) {
+    const msg = await message.reply("Pinging...");
+    msg.edit(`🏓 Pong! Latency: ${msg.createdTimestamp - message.createdTimestamp}ms`);
+  }
+};
