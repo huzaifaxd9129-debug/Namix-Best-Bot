b619f50ca476275ae25be64d541caa2774487563
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 20:57:31 +0500

    roleinfo.js

--- commands/utility/roleinfo.js
+++ commands/utility/roleinfo.js
@@ -1,1 +1,21 @@
+const { EmbedBuilder } = require("discord.js");
 
+module.exports = {
+  name: "roleinfo",
+  description: "Role information",
+
+  async execute(message, args) {
+    const role = message.mentions.roles.first();
+    if (!role) return message.reply("Mention a role!");
+
+    const embed = new EmbedBuilder()
+      .setTitle("Role Info")
+      .addFields(
+        { name: "Name", value: role.name },
+        { name: "Members", value: `${role.members.size}` }
+      )
+      .setColor(role.color);
+
+    message.reply({ embeds: [embed] });
+  }
+};
