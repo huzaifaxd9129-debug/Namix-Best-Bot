27266f7e9b2d0d972cbdb7dd1763b53c79a67006
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 22:05:35 +0500

    config.js

--- commands/welcome/config.js
+++ commands/welcome/config.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
+const file = "./data/welcome.json";
 
+function load() {
+  if (!fs.existsSync(file)) fs.writeFileSync(file, "{}");
+  return JSON.parse(fs.readFileSync(file, "utf8"));
+}
+
+module.exports = {
+  name: "welcome",
+
+  async execute(message, args) {
+    if (args[0] !== "config") return;
+
+    const data = load();
+    const config = data[message.guild.id];
+
+    if (!config)
+      return message.reply("❌ No welcome system found");
+
+    message.reply(
+      `📦 **Welcome Config**\n\nChannel: <#${config.channelId}>\nMessage: ${config.message}\nStatus: ${config.enabled ? "ON" : "OFF"}`
+    );
+  }
+};
