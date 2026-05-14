ec5d1e92d9d9ebabb517049a1bf93d235f8a789c
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 20:59:54 +0500

    serversplash.js

--- commands/utility/serversplash.js
+++ commands/utility/serversplash.js
@@ -1,1 +1,9 @@
+module.exports = {
+  name: "serversplash",
 
+  async execute(message) {
+    const splash = message.guild.splashURL({ size: 4096 });
+    if (!splash) return message.reply("No splash available.");
+    message.reply(splash);
+  }
+};
