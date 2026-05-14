513c476aa49df789af9cba72407ea9f1a53ed543
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 21:00:36 +0500

    snipe.js

--- commands/utility/snipe.js
+++ commands/utility/snipe.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: "snipe",
 
+  async execute(message, args, client) {
+    const data = client.snipes?.get(message.channel.id);
+    if (!data) return message.reply("Nothing to snipe.");
+
+    message.reply(`${data.author}: ${data.content}`);
+  }
+};
