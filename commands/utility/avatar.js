9e0848d8225711fa30d866e8ab5f0735fd8d4ca6
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-14 20:53:36 +0500

    avatar.js

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -1,1 +1,17 @@
+const { EmbedBuilder } = require("discord.js");
 
+module.exports = {
+  name: "avatar",
+  description: "Show user avatar",
+
+  async execute(message, args) {
+    const user = message.mentions.users.first() || message.author;
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+      .setColor("Blue");
+
+    message.reply({ embeds: [embed] });
+  }
+};
